f37037770a86f736151984080ab735a7ba35df1f
Rogier van Staveren <rogier@batoid.com>
2023-05-04 00:59:09 +0200

    Improved coordinator, ix integration reloading
    Retrieve source names from receiver
    Fix serial input buffer garbage

--- custom_components/nad/__init__.py
+++ custom_components/nad/__init__.py
@@ -21,7 +21,7 @@ from homeassistant.const import (
 from homeassistant.core import CALLBACK_TYPE, HomeAssistant, ServiceCall, callback
 from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers.entity import DeviceInfo
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 from nad_receiver import NADReceiver, NADReceiverTCP, NADReceiverTelnet
 
 from .const import (
@@ -36,7 +36,15 @@ _LOGGER = logging.getLogger(__name__)
 
 PLATFORMS: list[Platform] = [
     Platform.MEDIA_PLAYER,
+    Platform.NUMBER,
+    Platform.SWITCH,
+    Platform.SELECT,
+    Platform.SENSOR,
 ]
+
+
+class CommandNotSupportedError(Exception):
+    """Error to indicate a command is not supported."""
 
 
 class NADReceiverCoordinator(DataUpdateCoordinator):
@@ -48,9 +56,6 @@ class NADReceiverCoordinator(DataUpdateCoordinator):
     device_info: DeviceInfo = None
 
     power_state = None
-    muted = None
-    volume = None
-    source = None
 
     _listener_commands = []
 
@@ -65,7 +70,6 @@ class NADReceiverCoordinator(DataUpdateCoordinator):
             update_interval=timedelta(seconds=5),
         )
 
-        _LOGGER.debug(entry)
         self.config = entry.data
         self.options = entry.options
         self.receiver: NADReceiver = None
@@ -88,9 +92,8 @@ class NADReceiverCoordinator(DataUpdateCoordinator):
     async def connect(self):
         if not self.model:
             # Open the connection by requesting the model
-            self.model = self.receiver.main_model("?")
-            self.version = self.receiver.main_version("?")
-            self.power_state = self.receiver.main_power("?")
+            self.model = self.exec_command("Main.Model", "?")
+            self.version = self.exec_command("Main.Version", "?")
 
             self.device_info = DeviceInfo(
                 identifiers={(DOMAIN, self.unique_id)},
@@ -100,6 +103,8 @@ class NADReceiverCoordinator(DataUpdateCoordinator):
                 sw_version=self.version,
             )
 
+            self.sources = self.get_sources()
+
     async def disconnect(self):
         pass
 
@@ -107,41 +112,88 @@ class NADReceiverCoordinator(DataUpdateCoordinator):
     def async_add_listener(
         self, update_callback: CALLBACK_TYPE, context: Any = None
     ) -> Callable[[], None]:
-        super().async_add_listener(update_callback, context)
+        remove_listener = super().async_add_listener(update_callback, context)
 
         _LOGGER.debug("Adding listener for %s", context)
         if context:
-            if context not in self._listener_commands:
-                self._listener_commands.append(context)
+            self.add_listener_command(context)
 
-    def exec_command(self, command: str, action: str | None = None):
-        if action:
-            return self.receiver.exec_command(command, action)
-        return self.receiver.exec_command(command)
+        return remove_listener
+
+    def add_listener_command(self, command):
+        _LOGGER.debug("Adding command %s", command)
+        if command not in self._listener_commands:
+            self._listener_commands.append(command)
+
+    def get_sources(self) -> {}:
+        sources = {}
+
+        for i in range(1, 13):
+            try:
+                response = self.exec_command(f"Source{i}.Enabled", "?")
+                if response is not None and response.lower() == "yes":
+                    response = self.exec_command(f"Source{i}.Name", "?")
+                    sources[i] = response
+            except CommandNotSupportedError:
+                break
+
+        return sources
+
+    def supports_command(self, command: str):
+        try:
+            response = self.exec_command(command, "?")
+        except CommandNotSupportedError:
+            _LOGGER.debug("%s not supported", command)
+            return False
+
+        _LOGGER.debug("%s supported", command)
+        if not self.data:
+            self.data = {}
+        self.data[command] = response
+
+        return True
+
+    def exec_command(self, command: str, operator: str, value: Optional = None):
+        cmd = command + operator
+        if value:
+            cmd = f"{cmd}{value}"
+
+        if self.config[CONF_TYPE] == CONF_TYPE_SERIAL:
+            self.receiver.transport.ser.reset_input_buffer()
+
+        try:
+            msg = self.receiver.transport.communicate(cmd)
+            _LOGGER.debug("sent: '%s' reply: '%s'", cmd, msg)
+
+            if msg == "":
+                raise CommandNotSupportedError()
+
+            if msg.lower().startswith(command.lower() + "="):
+                return msg.split("=")[1]
+        except UnicodeDecodeError as ex:
+            _LOGGER.error(ex)
+
+        return None
 
     async def _async_update_data(self):
         """Fetch data from NAD Receiver."""
-        power_state = self.receiver.main_power("?")
+        power_state = self.exec_command("Main.Power", "?")
         _LOGGER.debug("power_state: %s", power_state)
         if not power_state:
             self.power_state = None
-            raise UpdateFailed(f"Error communicating with NAD Receiver")
+            raise UpdateFailed("Error communicating with NAD Receiver")
 
-        if power_state == "On":
+        if power_state.lower() == "on":
             self.power_state = MediaPlayerState.ON
         else:
             self.power_state = MediaPlayerState.OFF
 
         data = {}
+        data["Main.Power"] = power_state
 
-        if self.power_state == MediaPlayerState.ON:
-            self.muted = self.receiver.main_mute("?") == "On"
-            self.volume = self.receiver.main_volume("?")
-            self.source = self.receiver.main_source("?")
-
-            for command in self._listener_commands:
-                if command not in []:
-                    data[command] = self.exec_command(command, "?")
+        for command in self._listener_commands:
+            if command not in data:
+                data[command] = self.exec_command(command, "?")
 
         return data
 
@@ -158,10 +210,11 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     except serial.SerialException as ex:
         raise ConfigEntryNotReady(f"Unable to connect to NAD receiver: {ex}") from ex
 
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = receiver_coordinator
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = receiver_coordinator
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
+    entry.async_on_unload(entry.add_update_listener(options_update_listener))
 
     return True
 
@@ -175,3 +228,9 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
+
+
+async def options_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Handle options update."""
+    _LOGGER.debug("Configuration options updated, reloading NAD receiver integration")
+    await hass.config_entries.async_reload(entry.entry_id)

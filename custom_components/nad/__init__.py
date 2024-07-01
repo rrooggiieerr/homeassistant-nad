328b26895a487afe763d9e03cb8eb93ff2d5790d
Rogier van Staveren <rogier@batoid.com>
2024-07-01 14:01:39 +0200

    Rename options update listener

--- custom_components/nad/__init__.py
+++ custom_components/nad/__init__.py
@@ -222,7 +222,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
-    entry.async_on_unload(entry.add_update_listener(options_update_listener))
+    entry.async_on_unload(entry.add_update_listener(update_listener))
 
     return True
 
@@ -238,7 +238,7 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return unload_ok
 
 
-async def options_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
+async def update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
     """Handle options update."""
     _LOGGER.debug("Configuration options updated, reloading NAD receiver integration")
     await hass.config_entries.async_reload(entry.entry_id)
